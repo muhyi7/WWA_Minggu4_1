7fdca1ca90d07379d2cc83af84e015964e7b20f6
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-09-07 05:33:05 +0700

    update minor

--- example.3.php
+++ example.3.php
@@ -1,11 +1,11 @@
-<?php include("class_lib.php");?>
+<?php include("class_lib.php"); ?>
 <?php
     $stefan = new person();
-    $jimmy = new person;
+    $jimmy = new person();
 
     $stefan->set_name("Stefan Mischook");
     $jimmy->set_name("Nick Waddles");
 
-    // direct access
-    echo "Stefan's full name: ".$stefan->name;
+    // Menggunakan metode get_name untuk mengakses properti name
+    echo "Stefan's full name: " . $stefan->get_name();
 ?>
